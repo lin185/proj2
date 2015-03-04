91434f0400c118b6851a8a0835e501a920d0b05d
Binhao Lin <lin185@pal-nat184-210-72.itap.purdue.edu>
2015-03-03 19:21:29 -0500

    update

--- BufferManager/src/bufmgr/BufMgr.java
+++ BufferManager/src/bufmgr/BufMgr.java
@@ -2,12 +2,20 @@ package bufmgr;
 
 import global.PageId;
 import global.Page;
+import diskmgr.*;
+
+import java.io.IOException;
+import chainexception.ChainException;
 
 public class BufMgr {
 
 	int numbufs;
 	Page[] bufferPool;
 	String replacementPolicy;
+
+
+	DiskMgr diskManager;
+	
 	/**
 	* Create the BufMgr object.
 	* Allocate pages (frames) for the buffer pool in main memory and
@@ -21,29 +29,33 @@ public class BufMgr {
 		this.numbufs = numbufs;
 		bufferPool = new Page[numbufs];
 		this.replacementPolicy = replacementPolicy;
+		
+		diskManager = new DiskMgr();
 	}
-/**
-* Pin a page.
-* First check if this page is already in the buffer pool.
-* If it is, increment the pin_count and return a pointer to this
-* page.
-* If the pin_count was 0 before the call, the page was a
-* replacement candidate, but is no longer a candidate.
-* If the page is not in the pool, choose a frame (from the
-* set of replacement candidates) to hold this page, read the
-* page (using the appropriate method from {\em diskmgr} package) and pin it.
-* Also, must write out the old page in chosen frame if it is dirty
-* before reading new page.__ (You can assume that emptyPage==false for
-* this assignment.)
-*
-* @param pageno page number in the Minibase.
-* @param page the pointer point to the page.
-* @param emptyPage true (empty page); false (non­empty page)
-*/
-public void pinPage(PageId pageno, Page page, boolean emptyPage) {
 	
 	
-}
+	/**
+	* Pin a page.
+	* First check if this page is already in the buffer pool.
+	* If it is, increment the pin_count and return a pointer to this
+	* page.
+	* If the pin_count was 0 before the call, the page was a
+	* replacement candidate, but is no longer a candidate.
+	* If the page is not in the pool, choose a frame (from the
+	* set of replacement candidates) to hold this page, read the
+	* page (using the appropriate method from {\em diskmgr} package) and pin it.
+	* Also, must write out the old page in chosen frame if it is dirty
+	* before reading new page.__ (You can assume that emptyPage==false for
+	* this assignment.)
+	*
+	* @param pageno page number in the Minibase.
+	* @param page the pointer point to the page.
+	* @param emptyPage true (empty page); false (non­empty page)
+	*/
+	public void pinPage(PageId pageno, Page page, boolean emptyPage) {
+		
+		
+	}
 /**
 * Unpin a page specified by a pageId.
 * This method should be called with dirty==true if the client has
@@ -74,9 +86,9 @@ public void unpinPage(PageId pageno, boolean dirty) {}
 *
 * @return the first page id of the new pages.__ null, if error.
 */
-public PageId newPage(Page firstpage, int howmany) {
-	PageId pid = null;
-	
+public PageId newPage(Page firstpage, int howmany)  throws IOException, ChainException{
+	PageId pid = new PageId();
+	diskManager.allocate_page(pid, howmany);
 	return pid;
 }
 /**
